8306029defea0e0f45fc22c1fe3e7f0f11cf8165
chenhan <chenhan@jixiang.cn>
2026-09-03 15:02:10 +0800

    Archive WSDoc dependencies batch p-r

---
+++ RXSDK/WSDoc/node_modules/remark-definition-list/lib/index.d.ts
@@ -0,0 +1,5 @@
+import type { Processor } from 'unified';
+export { defListHastHandlers } from 'mdast-util-definition-list';
+export { defListHastToMdast } from 'hast-util-definition-list';
+export declare function remarkDefinitionList(this: Processor): void;
+export default remarkDefinitionList;
